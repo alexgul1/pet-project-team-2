f613f8bfb905d5b6ab8f20e577cf207c8f0b8ee7
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2020-03-18 16:47:33 +0200

    add

--- web-socket-chat/script.js
+++ web-socket-chat/script.js
@@ -1,4 +1,8 @@
 const socket = new WebSocket('wss://zi-node-chat.herokuapp.com');
+let containerChat = document.querySelector('.chat-container');
+let sendButton = document.getElementsByClassName('send-message');
+let userField = document.getElementsByClassName('user-field');
+let messageField = document.getElementsByClassName('message-field');
 
 let chatHistory = [];
 let currentUser;
@@ -9,6 +13,7 @@ socket.addEventListener('open', function (event) {
 
 socket.addEventListener('message', function (event) {
     parseData(event.data)
+    getChatHistory()
 });
 
 function parseData(data){
@@ -18,6 +23,7 @@ function parseData(data){
         chatHistory = parsedData.data;
     }else if(parsedData.type === "message"){
         chatHistory.push(parsedData);
+        addIncomingMessage(parsedData)
     }else if(parsedData.type === "color"){
         currentUser = parsedData;
     }
@@ -26,4 +32,33 @@ function parseData(data){
 function sendData(data) {
     socket.send(data)
 }
+
+
+function getChatHistory() {
+    debugger
+    for(let i = 0; i < chatHistory.length; i++) {
+        const chatMessage = document.createElement('div')
+        const spanAuthor = (`<span style="color: ${chatHistory[i].color}">${chatHistory[i].author}</span>`)
+        chatMessage.innerHTML = `${spanAuthor} @ ${chatHistory[i].time} : ${chatHistory[i].text}`
+        containerChat.append(chatMessage);
+    }
+}
+
+
+function addIncomingMessage(message) {
+    debugger
+    const chatMessage = document.createElement('div')
+    const spanAuthor = (`<span style="color: ${message.color}">${message.author}</span>`)
+    chatMessage.innerHTML = `${spanAuthor} @ ${message.time} : ${message.text}`
+    containerChat.append(chatMessage);
+}
+
+sendButton.addEventListener('click', () => {
+    if(userField.value) {
+
+    }
+    if(messageField.value) {
+
+    }
+})
 
