220f32e5e5fc252dea97d757def6a17dcb30ce78
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2020-03-18 13:52:10 +0200

    fixes

--- projects-page.js
+++ projects-page.js
@@ -1,6 +1,6 @@
 let iframe = document.getElementById('iframe-our-projects');
 
-let link = document.querySelector('.projects-header-menu__link');
+let link = document.getElementsByClassName('projects-header-menu__link');
 let container = document.getElementsByClassName('container-row');
 
 let sources = ['rest-api/balloons.html','crud/build/index.html', 'landing-page/index.html'];
@@ -9,6 +9,8 @@ function changeIframe(index) {
     iframe.src = sources[index];
 }
 for(let i = 0; i < link.length; i++) {
-    link[i].addEventListener('click', changeIframe(i))
+    link[i].addEventListener('click', () => {
+        changeIframe(i);
+    })
 }
 
