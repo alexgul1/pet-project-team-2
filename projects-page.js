dc23473af0a1b94e8ebdd0e48d508b1f1ea1a76b
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2020-03-19 17:19:30 +0200

    update css piroll

--- projects-page.js
+++ projects-page.js
@@ -3,7 +3,7 @@ let iframe = document.getElementById('iframe-our-projects');
 let link = document.getElementsByClassName('projects-header-menu__link');
 let container = document.getElementsByClassName('container-row');
 
-let sources = ['rest-api/balloons.html','crud/build/index.html', 'landing-page/index.html'];
+let sources = ['rest-api/balloons.html','crud/build/index.html', 'landing-page/index.html', 'web-socket-chat/index.html'];
 
 function changeIframe(index) {
     iframe.src = sources[index];
